a885c1eb4249812e4d3695a138e53fb1db62e669
Will Asciutto <will.asciutto@elderresearch.com>
2021-03-14 19:07:39 -0400

    Initial commit

---
+++ sql/create_tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Staff (
+    staffID CHAR(64),
+    storeID CHAR(64),
+    name VARCHAR(128),
+    age INTEGER,
+    homeAddress VARCHAR(128),
+    jobTitle VARCHAR(64),
+    phoneNumber VARCHAR(16),
+    emailAddress VARCHAR(64),
+    timeOfEmployment TIMESTAMP
+)
