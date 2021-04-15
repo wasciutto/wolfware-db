e8cdcb59bf17120e799ad19aa46362825e4c4213
Bhavya Agrawal <agrawal.bhavya8@gmail.com>
2021-04-15 18:39:53 -0400

    Staff and Store

---
+++ db/enterStoreInfo.java
@@ -0,0 +1,115 @@
+import java.sql.*;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class enterStoreInfo {
+
+
+// Update your user info alone here
+private static final String jdbcURL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/bagrawa"; // Using SERVICE_NAME
+
+// Update your user and password info here!
+
+private static final String user = "bagrawa";
+private static final String password = "200347899";
+
+public static void main(String[] args) {
+try {
+// Loading the driver. This creates an instance of the driver
+// and calls the registerDriver method to make MySql(MariaDB) Thin available to clients.
+
+
+Class.forName("org.mariadb.jdbc.Driver");
+
+            Connection connection = null;
+            Statement statement = null;
+			ResultSet result = null;
+            
+			String storeID = null;
+            String sqlSelect = null;
+			      Scanner sc= new Scanner(System.in);
+            try {
+            // Get a connection instance from the first driver in the
+            // DriverManager list that recognizes the URL jdbcURL
+            connection = DriverManager.getConnection(jdbcURL, user, password);
+            statement = connection.createStatement();
+            // Create a statement instance that will be sending
+            // your SQL statements to the DBMS
+            
+      			try{
+				System.out.print("Enter store ID (Please enter ID in quotes): "); 
+      			storeID = sc.nextLine();
+				
+				System.out.print("Enter manager ID (Please enter ID in quotes): "); 
+      			String managerID = sc.nextLine();
+      			
+				System.out.print("Enter store address in quotes");
+				String storeAddr= sc.nextLine();
+				
+				System.out.print("Enter phone number in quotes");
+				String phoneNumber= sc.nextLine();
+				
+      			String sql= "INSERT INTO STORE VALUES (%s,%s,%s,%s)";
+				sqlSelect = String.format(sql, storeID, managerID, storeAddr, phoneNumber);
+			
+			
+            }
+            catch(Throwable oops) {
+              System.out.print("Incorrect format for Store Id");
+            }
+			
+			try{
+			connection.setAutoCommit(false);
+  			result = statement.executeQuery(sqlSelect);
+			connection.commit();
+			System.out.println("Statement Executed");
+          }
+          System.out.println("*************************************************");
+        }
+        catch (Exception e){
+		connection.rollback();
+        System.out.println("Statement not executed");
+        
+        }
+			
+			} finally {
+                close(result);                  
+                close(statement);
+                close(connection);
+                
+            }
+} catch(Throwable oops) {
+            oops.printStackTrace();
+        }
+}
+static void close(Connection connection) {
+        if(connection != null) {
+            try {
+            connection.close();
+            } catch(Throwable whatever) {}
+        }
+    }
+    
+static void close(Statement statement) {
+        if(statement != null) {
+            try {
+            statement.close();
+            } catch(Throwable whatever) {}
+        }
+    }
+    
+ static void close(ResultSet result) {
+        if(result != null) {
+            try {
+            result.close();
+            } catch(Throwable whatever) {}
+        }
+    }
+    
+    
+}
