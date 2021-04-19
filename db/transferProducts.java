356c08f094357f342a92f28c4cc6b043417a9435
Unnati Nadupalli <prema.unnati@gmail.com>
2021-04-19 19:10:39 -0400

    final product ineventory api

--- db/transferProducts.java
+++ db/transferProducts.java
@@ -1,3 +1,4 @@
+//importing all packages required
 import java.sql.*;
 import java.util.Scanner;
 import java.util.InputMismatchException;
@@ -9,23 +10,22 @@ import java.sql.Statement;
 
 public class transferProducts {
 
+	//adding database url
+	private static final String jdbcURL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/pattri";
+	//loading database credentials
+	private static final String user = "pattri";
+	private static final String password = "200226336";
 
-
-private static final String jdbcURL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/upnadupa"; 
-
-
-private static final String user = "upnadupa";
-private static final String password = "Momos";
 
 public static void main(String[] args) {
 try {
 
-
+//register jdbc driver 
 Class.forName("org.mariadb.jdbc.Driver");
-
+			//creating required variables.
             Connection connection = null;
             Statement statement = null;
-           
+			ResultSet result = null;
             
 			String productID = null;
 			String batchID = null;
@@ -38,12 +38,13 @@ Class.forName("org.mariadb.jdbc.Driver");
 			Scanner sc= new Scanner(System.in);
             
 			try {
-            
+            //opening a connection
             connection = DriverManager.getConnection(jdbcURL, user, password);
             statement = connection.createStatement();
             
             
       			try{
+					//taking the required inputs from the command line to execute queries 
 					System.out.print("Enter Your Staff ID:"); 
         			staffID = sc.nextLine();
         			
@@ -59,7 +60,7 @@ Class.forName("org.mariadb.jdbc.Driver");
 					System.out.print("Enter Store ID of store to transfer product to:"); 
         			storeID2 = sc.nextLine();
         			
-					
+					//creating sql statements and inserting input values into them 
 					String sql = "UPDATE HASPRODUCTINVENTORY SET STOREID=%s  WHERE PRODUCTID=%s AND STOREID=%s AND BATCHID=%s";
 					sqlSelect1 = String.format(sql, "'"+storeID2+"'", "'"+productID+"'","'"+storeID1+"'","'"+batchID+"'");
 					
@@ -73,20 +74,47 @@ Class.forName("org.mariadb.jdbc.Driver");
               System.out.print("Data inserted was in incorrect format.");
             }
 			try{
+				//setting auto commit to false to prevent any changes made to the database incase of an error.
 				connection.setAutoCommit(false);
+				//executing the sql queries.
 				statement.executeQuery(sqlSelect1);
 				statement.executeQuery(sqlSelect2);
-				statement.executeQuery(sqlSelect3);
-				statement.executeQuery(sqlSelect4);
+				//commiting the changes made to the database once both queries have run. 
+				
 				connection.commit();
-				System.out.println("Product was transfer is complete. :) ");
+				System.out.println("Product transfer is complete. :) ");
+				
 				}catch (Exception e){
+				//rolling back all the changes made to the database if an error or a failure occurs. 
 				connection.rollback();
-				System.out.println("Product transfer between stores failed.");
+				System.out.println("Product transfer between stores failed.\n");
+				
+				try{
+					//retrieving the result once the transaction is complete. 
+					//slq statement for retrieving entire Product inventory table 
+					String sqlresult="SELECT * FROM HASPRODUCTINVENTORY";
+					//running the statement to get result. 
+					result= statement.executeQuery(sqlresult);
+					
+					//parsing through the ResultSet one by one and displaying all the values. 
+					
+					while(result.next())
+					{
+						String pid= result.getString("PRODUCTID");
+						String bid= result.getString("BATCHID");
+						String sid= result.getString("STOREID");
+						
+						System.out.print("Product id: "+pid);
+						System.out.print("Batch id: "+bid);
+						System.out.print("Store id: "+sid);
+					}
+					}catch(Exception e){
+					System.out.println("error in printing the modified changes");}
 				}	
 				System.out.println("*************************************************");
 			}finally {
-                           
+				//closing all resources
+                close (result)
                 close(statement);
                 close(connection);
                 
